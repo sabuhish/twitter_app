796d4fe7e7fe63efc9d55e21c7764a7670c34c48
munisisazade <munisisazade@gmail.com>
2019-01-15 11:06:01 +0400

    Login page ready

--- twitter_app/urls.py
+++ twitter_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.baseindexview, name="home"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
     path('dashboard/', views.dashboard_view, name="dashboard"),
     path('timeline/slug/', views.timeline_view, name="timeline"),
 ]
--- twitter_app/views.py
+++ twitter_app/views.py
@@ -1,11 +1,47 @@
-from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from twitter_app.forms import LoginForm
+
+
+def login_page_data():
+    return {
+        "login_form": LoginForm(),
+    }
+
+
 def baseindexview(request):
-    context = {}
+    if request.user.is_authenticated:
+        return redirect("dashboard")
+    context = login_page_data()
     return render(request, "accounts/login.html", context)
 
+
+def login_view(request):
+    if request.method == "POST":
+        context = login_page_data()
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user and user.is_active:
+                login(request, user)
+                return redirect("dashboard")
+            else:
+                context["message"] = "username or password invalid !"
+                return render(request, "accounts/login.html", context)
+        else:
+            context["login_form"] = form
+            return render(request, "accounts/login.html", context)
+    else:
+        return redirect("home")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
 
 def dashboard_view(request):
     context = {}
