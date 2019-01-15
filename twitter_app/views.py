a6db7291b3f50d790cfe9c39170720af09851563
Sebuhi Shukurov <sabuhi.shukurov@gmail.com>
2019-01-15 11:27:36 +0400

    test

--- twitter_app/views.py
+++ twitter_app/views.py
@@ -10,7 +10,7 @@ def login_page_data():
         "login_form": LoginForm(),
     }
 
-
+ #salam test
 def baseindexview(request):
     if request.user.is_authenticated:
         return redirect("dashboard")
